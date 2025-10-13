228de07ae19609657c07e315bd634f1349304da8
diaohongning <etodemerzel@qq.com>
2025-10-13 17:06:53 +0800

    Initial Commit

---
+++ DemoApp&gitTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DemoApp&gitTest
+//
+//  Created by 刁泓宁 on 2025/10/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
